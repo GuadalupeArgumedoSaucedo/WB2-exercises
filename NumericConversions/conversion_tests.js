908f46df9fe0d87752495dbb3c8d76b8f4659171
GuadalupeArgumedoSaucedo <lupeargumedo96@gmail.com>
2024-04-25 11:30:44 -0700

    numeric conversions
    numeric conversions

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,54 @@
+"use strict"
+
+// Description:  This script tests various numeric 
+// conversion techniques 
+// Author: Jordan Q. Newprogrammer 
+
+let a = "101.1"; 
+let b = "55"; 
+let c = "402 Stevens"; 
+let d = "Number 5"; 
+
+// parseInt
+let ai = parseInt(a);
+let bi = parseInt(b);
+let ci = parseInt(c);
+let di = parseInt(d);
+
+console.log(ai)
+console.log(bi)
+console.log(ci)
+console.log(di)
+
+//ParseFloat
+let af = parseFloat(a);
+let bf = parseFloat(b);
+let cf = parseFloat(c);
+let df = parseFloat(d);
+
+console.log(af)
+console.log(bf)
+console.log(cf)
+console.log(df)
+
+//Number
+let an = Number(a);
+let bn = Number(b);
+let cn = Number(c);
+let dn = Number(d);
+
+console.log(an)
+console.log(bn)
+console.log(cn)
+console.log(dn)
+
+//unary +
+let au = +a;
+let bu = +b;
+let cu = +c;
+let du = +d;
+
+console.log(au)
+console.log(bu)
+console.log(cu)
+console.log(du)
