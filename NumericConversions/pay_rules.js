b00216497b698255d94711d17a6c8764e9e04880
GuadalupeArgumedoSaucedo <lupeargumedo96@gmail.com>
2024-04-25 14:17:38 -0700

    pay

---
+++ NumericConversions/pay_rules.js
@@ -0,0 +1,10 @@
+let payrate = 30;
+let hoursworked = 40;
+
+let overtime = 1.5;
+let overtimeworkerd = hoursworked*overtime;
+
+
+if(hoursworked > 40) {
+    console.log(`your overtime pay is ${overtimeworkerd}`);
+}
